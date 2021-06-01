7fbc17517a0d0eccf4395ce35ef4ceecd8245e5e
AxtraI <73775548+AxtraI@users.noreply.github.com>
2021-06-01 21:51:22 +0800

    Add files via upload

---
+++ перевернутьматрицу.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int** x = new int* [n];
+    for (int i = 0; i < n; i++)
+        x[i] = new int[m];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> x[i][j];
+    cout << m << " " << n;
+    cout << endl;
+    for (int j = 0; j < m; j++) {
+        for (int i = n - 1; i >= 0; i--)
+            cout << x[i][j] << " ";
+        cout << endl;
+    }
+    return 0;
+}
